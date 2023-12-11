1fe8b4ae03f9b46c5d5b4c3fb6217ee97ea7d83a
TreeBronch <74438588+TreeBronch@users.noreply.github.com>
2023-12-11 16:14:50 -0500

    Changes by justin

--- src/main/httpserverhandler.py
+++ src/main/httpserverhandler.py
@@ -1,14 +1,17 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
+
 class JSONHandler(BaseHTTPRequestHandler):
     def __init__(self, request, client_address, server, proc_server):
         self.proc_server = proc_server
         super().__init__(request, client_address, server)
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        code, text, contenttype, method, h2, h2content = self.proc_server.handle_post(post_data, self.headers, self.path)
+        code, text, contenttype, method, h2, h2content = self.proc_server.handle_post(post_data, self.headers,
+                                                                                      self.path)
         self.send_response(code)
         self.send_header('Content-type', contenttype)
         if h2 is not None and h2content is not None:
@@ -22,6 +25,7 @@ class JSONHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', contenttype)
         self.end_headers()
         self.wfile.write(data.encode('utf-8'))
+
 
 class ClosableHTTPServer(HTTPServer):
     def __init__(self, address, handler, parent_server):
