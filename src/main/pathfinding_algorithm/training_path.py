ac5474f2f737cebd4247e2572ec0807177bcb12b
TreeBronch <74438588+TreeBronch@users.noreply.github.com>
2023-12-17 18:04:59 -0500

    Started adding dstarlite with justin

--- src/main/pathfinding_algorithm/training_path.py
+++ src/main/pathfinding_algorithm/training_path.py
@@ -28,7 +28,17 @@ def update_vertex(graph, queue, id, current_state, k_m):
     id_in_queue = [item for item in queue if id in item]
     if id_in_queue:
         if len(id_in_queue) != 1:
-            raise ValueError("")
+            raise ValueError(f"More than one {id} in the queue")
+        queue.remove(id_in_queue[0])
+    if graph.graph[id].rhs != graph.graph[id].g:
+        heapq.heappush(queue, calc_key(graph, id, current_state, k_m) + (id,))
+
+
+def shortest_path(graph, queue, start_state, k_m):
+    while graph.graph[start_state].rhs != graph.graph[start_state].g or topKey(queue) < calc_key(graph, start_state, start_state, k_m):
+        k_old = topKey(queue)
+        u = heapq.heappop(queue)[2]
+        if k_old < calc_key(graph, u, start_state, k_m):
 
 
 
